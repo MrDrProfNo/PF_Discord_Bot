695a6ba92204c64999485eaac47f0e599c55efc8
aheiles <32115458+MrDrProfNo@users.noreply.github.com>
2020-09-02 14:54:16 -0400

    Added initial file

---
+++ bot_core.py
@@ -0,0 +1,48 @@
+import discord
+from discord.ext import commands
+
+
+bot = commands.Bot(command_prefix='!')
+
+
+@bot.event
+async def on_connect():
+    print("Connected")
+
+
+@bot.event
+async def on_ready():
+    print("Ready")
+
+
+@bot.event
+async def on_disconnect():
+    print("Disconnect")
+
+
+@bot.command()
+async def echo(context: commands.Context, *args):
+    print(context)
+    print(args)
+    await context.send("args: " + str(args) + "\n", embed="test")
+
+@bot.command()
+async def scrims(context: commands.Context, *args):
+    if len(args) > 0:
+        await context.send("args received but not yet processed")
+
+    message_embed = discord.Embed()
+
+    message_embed.title = ":tools: CREATE GAME"
+
+    message_embed.description = \
+        "React below :arrow_forward: and we'll start setting up your game\nMake sure your DMs are on"
+
+    message_embed.set_thumbnail(
+        url="https://i.imgur.com/J6wmi3U.png%22%7D,%22color%22:4886754%7D"
+    )
+
+    await context.send(embed=message_embed)
+
+
+bot.run("NDY1MzQ4NDYwMzkzMDA1MDY3.W0F7hg.28wUqhIwSMTMgZ6cStDXs38qBnQ")
