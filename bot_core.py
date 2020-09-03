11103db63ce210c0dbfee14fc190906fd1f41b3b
aheiles <32115458+MrDrProfNo@users.noreply.github.com>
2020-09-02 21:08:46 -0400

    Added documentation

--- bot_core.py
+++ bot_core.py
@@ -2,6 +2,15 @@ import discord
 from discord.ext import commands
 import sys
 
+
+# this value obtained using:
+# import unicodedata
+# unicodedata.name('▶')
+# where ▶ was obtained by escaping the first colon of the emoji text code, and
+# copy-pasting the resultant output.
+UNICODE_FORWARD_ARROW = "\N{BLACK RIGHT-POINTING TRIANGLE}"
+
+
 bot = commands.Bot(command_prefix='!')
 
 
@@ -44,9 +53,18 @@ async def scrims(context: commands.Context, *args):
         url="https://i.imgur.com/J6wmi3U.png%22%7D,%22color%22:4886754%7D"
     )
 
-    message_embed.colour
+    # context.send returns a Message for the content it just sent
+    msg = await context.send(embed=message_embed)
 
-    await context.send(embed=message_embed)
+    # Message objects can be manipulated in a variety of ways for interaction
+    # with users
+    await msg.add_reaction(UNICODE_FORWARD_ARROW)
+
+    # TODO: message ID will come in useful later when tracking which message
+    #  was reacted to.
+    print("Message ID: " + str(msg.id))
+
+
 
 
 def main():
